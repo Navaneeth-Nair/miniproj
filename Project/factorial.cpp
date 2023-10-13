ab8323aa82d0f6996d7415e890e5cef4fc5dc015
Navaneeth Nair <navaneethnair82@gmail.com>
2023-10-13 23:01:03 +0530

    New One

---
+++ Project/factorial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ int x;
+ long factorial = 1.0;
+
+ cout<<"Enter Your Integer: ";
+ cin>>x;
+
+ if (x<0)
+ {
+    cout<<"A Factorial Cannot Be Calculated For Negative Integers.";
+    return 0;
+ }
+ else {
+    for (int i = 0; i <= x; ++i)
+    {
+        factorial *= i;
+    }
+    cout << "Factorial of " << x << " = " << factorial;     
+ }
